36dc82bfda03355dbcb3f04a9098817804dc64ca
wolfgang.jaede <wolfgang.jaede@bauerxcel.de>
2015-10-31 12:26:06 +0100

    helth_check & listener

--- main.tf
+++ main.tf
@@ -14,16 +14,16 @@ resource "aws_elb" "vpc" {
 
   listener {
     instance_port = "${var.listener.instance_port}"
-    instance_protocol = "http"
-    lb_port = 80
-    lb_protocol = "http"
+    instance_protocol = "${var.listener.instance_protocol}"
+    lb_port = "${var.listener.lb_portt}"
+    lb_protocol = "${var.listener.lb_protocol}"
   }
   health_check {
-    healthy_threshold = 2
-    unhealthy_threshold = 2
-    timeout = 3
-    target = "HTTP:80/"
-    interval = 10
+    healthy_threshold = "${var.health_check.healthy_threshold}"
+    unhealthy_threshold = "${var.health_check.unhealthy_threshold}"
+    timeout = "${var.health_check.}"
+    target = "${var.health_check.timeout}"
+    interval = "${var.health_check.interval}"
   }
 
   instances = ["${split(",", var.instance_ids)}" ]
