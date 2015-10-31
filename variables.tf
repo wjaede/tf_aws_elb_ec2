9a6f8eb416069ec14b6650e224785262afb59455
wolfgang.jaede <wolfgang.jaede@bauerxcel.de>
2015-10-31 12:11:18 +0100

    helth_check & listener

--- variables.tf
+++ variables.tf
@@ -7,3 +7,21 @@ variable "instance_ids"          {}
 variable "sg_ids"               {}
 variable "sbn_ids"              {}
 variable "app_name"             {}
+
+variable "listener"                 {
+  default = {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+}
+variable "health_check"             {
+  default = {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    target = "HTTP:80/"
+    interval = 10
+  }
+}
