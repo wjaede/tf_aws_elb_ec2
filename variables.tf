fe1e797ba23266928ccb805f16d5edd82c93b4a0
wolfgang.jaede <wolfgang.jaede@bauerxcel.de>
2015-10-31 12:43:48 +0100

    helth_check & listener

--- variables.tf
+++ variables.tf
@@ -21,7 +21,7 @@ variable "health_check"             {
     healthy_threshold = 2
     unhealthy_threshold = 2
     timeout = 3
-    target = "HTTP:80/"
+    target = "${var.health_check.target}"
     interval = 10
   }
 }
