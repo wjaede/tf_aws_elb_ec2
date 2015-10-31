fc2dfcd83ef89f495d3bc64b69fc08c3a3b4e2da
wolfgang.jaede <wolfgang.jaede@bauerxcel.de>
2015-10-31 12:21:55 +0100

    helth_check & listener

--- main.tf
+++ main.tf
@@ -11,10 +11,9 @@ resource "aws_elb" "vpc" {
   name = "${var.app_name}"
   subnets = [ "${split(",", var.sbn_ids)}" ]
   security_groups =  [ "${split(",", var.sg_ids)}" ]
-  listener = ["${var.listener}"]
-  health_check = ["${var.health_check}"]
-  /*listener {
-    instance_port = 80
+
+  listener {
+    instance_port = "${var.listener.instance_port}"
     instance_protocol = "http"
     lb_port = 80
     lb_protocol = "http"
@@ -25,7 +24,7 @@ resource "aws_elb" "vpc" {
     timeout = 3
     target = "HTTP:80/"
     interval = 10
-  }*/
+  }
 
   instances = ["${split(",", var.instance_ids)}" ]
 }
